52ef233e6056d9c066f3dd76cbe8b8b9f0e1b042
guidopro <169035608+guidopro@users.noreply.github.com>
2026-08-26 17:03:22 +0100

    Refactor Header and SignIn components for improved layout and functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-evenly p-2 bg-blue-900">
+    <header className="flex items-center justify-center gap-5 bg-blue-900 px-6 py-4">
       <Link to="/">
-        <img className="size-27 " src="/world_travel_icon_134840.webp" />
+        <img
+          className="size-20 object-contain"
+          src="/world_travel_icon_134840.webp"
+          alt="NC-News logo"
+        />
       </Link>
+
       <Link to="/">
-        <h1 className="inline text-6xl font-mono bg-amber-50 p-1">NC-News</h1>
+        <h1 className="bg-amber-50 px-4 py-2 font-mono text-5xl">NC-News</h1>
       </Link>
-    </div>
+    </header>
   );
 }
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,11 +3,29 @@ import { UserContext } from "../contexts/user";
 import { useContext } from "react";
 
 export default function SignIn() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+
+  function handleSignOut() {
+    setUser("");
+  }
+
   return (
-    <div className="flex flex-row-reverse bg-blue-950 text-white">
-      {!user && <Link to="sign-in">Sign in</Link>}
-      {user && <Link to="sign-in">Signed in as: {user.username}</Link>}
+    <div className="flex justify-end bg-blue-950 px-6 py-2 text-white">
+      {!user && (
+        <Link className="hover:underline" to="sign-in">
+          Sign in
+        </Link>
+      )}
+      {user && (
+        <>
+          <div className="flex items-center gap-3">
+            <span>Signed in as: {user.username}</span>
+            <button className="hover:underline" onClick={handleSignOut}>
+              Sign out
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
