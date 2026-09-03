e4c4edfc2211a1bb28617f5d40a8ce8dca89ea69
guidopro <169035608+guidopro@users.noreply.github.com>
2026-09-03 12:16:05 +0100

    Update ScrollToTop component to reset scroll position on route change with search params

--- src/components/helper/ScrollToTop.jsx
+++ src/components/helper/ScrollToTop.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, search]);
 
   return null;
 }
